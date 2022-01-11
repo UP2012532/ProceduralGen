45a22e4eab67a9a69bf603ebb73965fe2696aa12
UP2012532 <91748140+UP2012532@users.noreply.github.com>
2022-01-11 20:51:40 +0000

    Further setup completed

---
+++ README.md
@@ -0,0 +1,13 @@
+# Procedural Generator
+
+## Introduction
+
+The goal of this project is to visualise seeded random functions in a graphical way.
+
+## Features
+
+* [X] Create Window
+* [ ] Draw grid
+* [ ] Draw Cells
+* [ ] Create Random Logic
+* [ ] Enable navigation of the area
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.awt.Dimension;
+import javax.swing.JFrame;
+
 class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        JFrame frame = new JFrame("Hello World");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.setSize(new Dimension(400, 400));
+        frame.setVisible(true);
     }
 }
