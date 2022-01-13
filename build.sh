bc86f27f85c5dd35601b0415dfd79dce45e731c3
UP2012532 <91748140+UP2012532@users.noreply.github.com>
2022-01-13 23:15:47 +0000

    Changed build folder name to bin

--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
 #Builds .java files into .class then creates a .jar with them all
 
 #If build folder exists: delete files in build folder (there may be unwanted class files)
-[ -d "build" ] && rm -r build/*
+[ -d "bin" ] && rm -r bin/*
 
 #Create .class files
 cd src
-javac -verbose Main.java -d ../build
+javac -verbose Main.java -d ../bin
 
 printf "\nClasses built now creating jar.\n\n"
 
 #Create .jar file
-cd ../build
+cd ../bin
 jar -cvfm ../App.jar ../manifest.txt *.class
