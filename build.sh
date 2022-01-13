c24e2f85b48e342796fb98cf5add2b7e028021c2
UP2012532 <91748140+UP2012532@users.noreply.github.com>
2022-01-13 23:13:08 +0000

    Updated build script

--- build.sh
+++ build.sh
@@ -1,7 +1,14 @@
+#Builds .java files into .class then creates a .jar with them all
+
+#If build folder exists: delete files in build folder (there may be unwanted class files)
+[ -d "build" ] && rm -r build/*
+
+#Create .class files
 cd src
-javac -verbose Main.java -d ../bin
+javac -verbose Main.java -d ../build
 
 printf "\nClasses built now creating jar.\n\n"
 
-cd ..
-jar -cvfm App.jar manifest.txt -C bin .
+#Create .jar file
+cd ../build
+jar -cvfm ../App.jar ../manifest.txt *.class
