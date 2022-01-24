76dd621650ca19f21c81d55786300a52802a8582
UP2012532 <91748140+UP2012532@users.noreply.github.com>
2022-01-24 04:00:25 +0000

    Switched to using lehmer random algorithm

--- src/RandomLogic.java
+++ src/RandomLogic.java
@@ -1,4 +1,3 @@
-import java.util.Random;
 import java.awt.Color;
 
 /**
@@ -6,28 +5,45 @@ import java.awt.Color;
 */
 public class RandomLogic {
 
-    private Random random;
-
-    //Constructor.
-    public RandomLogic() {
-        random = new Random();
-    }
+    private long nLehmer = 0;
 
     /**
-    * Given a coordinate returns a semi randomised seed to be used later (Based on PRNG).
+    * Given a coordinate returns a semi randomised seed to be used later.
     *
     * @param  x  The x coordinate of the cell.
     * @param  y  The y coordinate of the cell.
     * @return Returns the seed for the given seed.
     */
-    public int getSeed(int x, int y) {
-        final int a = 1664525;
-        final int c = 1013904223; //1013904223
-        final int m = (int)Math.pow(2, 32);
+    public long getSeed(int x, int y) {
+        return (x & 0xFFFF) << 16 | (y & 0xFFFF);
+    }
 
-        final int inSeed = (x << y) + (x + y);
-            
-        return ((a * inSeed) + c) % m;
+    /**
+    * Based on Lehmer random number generator.
+    *
+    * https://en.wikipedia.org/wiki/Lehmer_random_number_generator
+    *
+    * @return Returns the randomised long.
+    */
+    private long Lehmer() {
+        nLehmer += 0xe120fc15;
+        long tmp;
+        tmp = (long)nLehmer * 0x4a39b70d;
+        long m1 = (tmp >> 32) ^ tmp;
+        tmp = (long)m1 * 0x12fad5c9;
+        long m2 = (tmp >> 32) ^ tmp;
+        return m2;
+    }
+
+    /**
+    * Given a coordinate returns a colour for the cell.
+    *
+    * @param  min  The minimum integer to generate (inclusive).
+    * @param  max  The maximum integer to generate (exclusive).
+    * @return The randomly generated number.
+    */
+    private int randInt(int min, int max) {
+        return (int)((Lehmer() % (max - min)) + min);
     }
 
     /**
@@ -38,16 +54,15 @@ public class RandomLogic {
     * @return Colour the specified cell should be.
     */
     public Color getCell(int x, int y) {
-
-        random.setSeed(getSeed(x, y));
+        nLehmer = getSeed(x, y);
 
         if (x == 0 && y == 0) return new Color(255, 0, 0);
 
-        switch (random.nextInt(2)) {
+        switch (randInt(0, 2)) {
             case 0: //Green
-                return new Color(0, random.nextInt(256), 0);
+                return new Color(0, randInt(0, 256), 0);
             case 1: //Blue
-                return new Color(0, 0, random.nextInt(256));
+                return new Color(0, 0, randInt(0, 256));
             default:
                 return new Color(255, 255, 255);
         }
